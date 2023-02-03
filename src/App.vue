6b44731871ce3cc5b7afc78c14fee9899d45d84b
Maximiliano <maximartins45@gmail.com>
2023-02-03 01:45:26 -0300

    utilizando variables reactivas con ref

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,52 @@
 <template>
-  <div>
-    <h1>Bienvenidos a </h1>
-  </div>
-  <div>
-  <p :style="{color:colorText}" > {{ msg }} </p>
-    <img src="./assets/vue.svg" class="logo" alt="Vue logo" />
-  </div>
-  <div>
-    <h2> Los saluda {{ autor }}</h2>
+  <div class="container" :style="{backgroundColor:bgColor,color:textColor}">
+      <div>
+        <h1>Bienvenidos a </h1>
+    <p> {{ msg }} 
+      <img src="./assets/vue.svg" class="logo" alt="Vue logo" />
+    </p>
+    <h2> Los saluda <span>{{ autor }} </span> </h2>
+      <button style="margin: 1rem; padding:0.5rem; ;" @click="cambiarColor()" >Cambiar color</button>
+      </div>
   </div>
 </template>
 
 <!-- <script setup> para que dentro de el script me ahorro poner la importacion -->
 <script setup>
+import {ref} from "vue" // sirve para crear una variable reactiva y se recargue cuando se modifica la variable
   const msg = `Mi primer "Hola Mundo" en `.toUpperCase()
   const autor = "Maxi Martins"
-  const colorText = "green"
+  let bgColor = ref("white")
+  let textColor = ref("black")
+  const cambiarColor = () =>{
+    if(bgColor.value === "white"){
+      bgColor.value = "black"
+      textColor.value = "white"
+    }else{
+      bgColor.value = "white"
+      textColor.value = "black"
+    }
+  }
 </script>
 <!-- <style scoped>  para que los estilos solo afecte a este componente -->
-<style scoped>
-
-div{
-  font-family: 'Courier New', Courier, monospace;
+<style >
+*{
   margin: 0;
   padding: 0;
+}
+.container{
+  width: 100%;
+  height: 100vh;
+  font-family: 'Courier New', Courier, monospace;
+  margin: 0;
+  padding:0;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-weight: bolder;
-}
-p{
-  font-size: 2rem;
-}
-.logo {
-  margin:1rem ;
-  width: 3rem;
-  height: auto;
 }
 
+.logo {
+  width: 2rem;
+  height: auto;
+}
 </style>
